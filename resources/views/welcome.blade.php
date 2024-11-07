3fc0d5bf02f73ce442005bc99008bc81120e878a
piscopo luigi <gino-piscopo-@live.it>
2024-11-07 17:37:26 +0100

    personaggi

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+
+    <div class="container">
+        <div class="row">
+            @foreach ($dragon as $drago)
+                <div class="col-12 col-md-6">
+                     <x-card :drago="$drago" />
+                    
+                </div>
+            @endforeach
+        </div>
+    </div>
+ 
+
+   
+</x-layout>
